4688a7390efb27596805eb9c59028ccca3645e37
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-02-19 16:22:23 +0100

    javadoc

--- io/EntradaSalida.java
+++ io/EntradaSalida.java
@@ -1,35 +1,71 @@
 package com.ejemplo.io;
 
 import java.util.Scanner;
-
+/**
+ * Libreria Entrada Salida
+ *
+ * @author Santiago Romero
+* */
 public class EntradaSalida {
 
     private static final Scanner scanner = new Scanner(System.in);
-
+    /**
+     * Solicita al usuario que introduzca una cadena.
+     *
+     * @param mensaje El mensaje que se mostrará al usuario antes de solicitar la entrada.
+     * @return La cadena introducida por el usuario.
+     */
     public static String pedirString(String mensaje) {
         System.out.println(mensaje);
         return scanner.nextLine();
     }
 
+    /**
+     * Solicita al usuario que introduzca un entero.
+     *
+     * @param mensaje El mensaje que se mostrará al usuario antes de solicitar la entrada.
+     * @return El entero introducido por el usuario.
+     */
     public static int pedirInt(String mensaje) {
         System.out.println(mensaje);
         return scanner.nextInt();
     }
 
+    /**
+     * Solicita al usuario que introduzca un float.
+     *
+     * @param mensaje El mensaje que se mostrará al usuario antes de solicitar la entrada.
+     * @return El float introducido por el usuario.
+     */
     public static float pedirFloat(String mensaje) {
         System.out.println(mensaje);
         return scanner.nextFloat();
     }
-
+    /**
+     * Muestra una cadena en la consola.
+     *
+     * @param mensaje La cadena que se mostrará en la consola.
+     */
     public static void mostrarString(String mensaje) {
         System.out.println(mensaje);
     }
 
+    /**
+     * Muestra un entero en la consola.
+     *
+     * @param numero El entero que se mostrará en la consola.
+     */
     public static void mostrarInt(int numero) {
-        System.out.println(numero);
+        System.out.println(Integer.toString(numero));
     }
 
+    /**
+     * Muestra un float en la consola.
+     *
+     * @param numero El float que se mostrará en la consola.
+     */
     public static void mostrarFloat(float numero) {
-        System.out.println(numero);
+        System.out.println(Float.toString(numero));
     }
-}
+
+ }
--- operaciones/LibreriaOperacionesMain.java
+++ operaciones/LibreriaOperacionesMain.java
@@ -2,10 +2,20 @@ package com.ejemplo.operaciones;
 
 import com.ejemplo.io.EntradaSalida;
 
+/**
+ * Clase que proporciona un método main para realizar operaciones matemáticas básicas.
+ *
+ * @author Santiago Romero
+ */
 public class LibreriaOperacionesMain {
 
+    /**
+     * Método principal de la aplicación que hace uso de las demas librerias.
+     *
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
-        
+
         String num1Str = EntradaSalida.pedirString("Introduce el primer número:");
         Float num1 = Float.parseFloat(num1Str);
 
